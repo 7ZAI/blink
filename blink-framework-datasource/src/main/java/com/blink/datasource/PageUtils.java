a9605e4cb7c15e542748bf65237f84ac9ca0d1e9
binblink <353294273@qq.com>
2025-11-13 12:56:39 +0800

    datasource 完善分页泛型 完善代码模板提交

--- blink-framework-datasource/src/main/java/com/blink/datasource/PageUtils.java
+++ blink-framework-datasource/src/main/java/com/blink/datasource/PageUtils.java
@@ -19,15 +19,18 @@ public class PageUtils {
      * @param selectSql ExecuteFunction 传入执行语句 如 mapper.selectXXX();
      * @param r 响应实际业务对象
      * @return 响应实体对象
-     * @param <R>
-     * @param <P>
+     * @param <T> sql 返回的实体类型
+     * @param <R> 返回类型
+     * @param <P> 分页参数类型
      */
-    public static < R extends PageDTO,P extends PageDTO> R queryPage(P page, ExecuteFunction selectSql, R r){
+    public static  <T,R extends PageDTO<T>,P extends PageDTO<T>> R queryPage(P page, ExecuteFunction selectSql, R r){
 
         PageHelper.orderBy(page.getOrderBy());
-        PageInfo<Object> pageInfo  =  PageHelper.startPage(page.getPageNum(), page.getPageSize(),!(page.getTotal()==-1))
+        PageInfo<T> pageInfo  =  PageHelper.startPage(page.getPageNum(), page.getPageSize(),!(page.getTotal()==-1))
                 .doSelectPageInfo(selectSql::execute);
+        //分页结果设置进入真正的返回值中
         BeanUtil.copyProperties(pageInfo,r);
+        //设置值进入row
         r.setRows(pageInfo.getList());
 
         return r;
