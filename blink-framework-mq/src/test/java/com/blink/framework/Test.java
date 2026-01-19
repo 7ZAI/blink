34af43adcb916b082fa57ef0f34bae6d26458a8e
binblink <353294273@qq.com>
2026-01-19 23:52:44 +0800

    删改过编译

--- blink-framework-mq/src/test/java/com/blink/framework/Test.java
+++ blink-framework-mq/src/test/java/com/blink/framework/Test.java
@@ -10,29 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class Test {
 
     @Autowired
-    private MqOperatorComponet rabbitMQComponet;
-
-    @Autowired
     private MessageProdutor mqMessageSender;
-
-    @org.junit.jupiter.api.Test
-    public void test() throws Exception {
-
-
-        Person p = new Person();
-        p.setAge(18);
-        p.setName("Mike");
-        p.setMale(1);
-
-        MqGenericDTO<Person> mqGenericDTO = new MqGenericDTO();
-
-        mqGenericDTO.setBody(p);
-        mqGenericDTO.setExchange(MqConstant.DEFAULT_EXCHANGE_NAME);
-        mqGenericDTO.setRoutingKey(MqConstant.DEFAULT_ROUTING_KEY);
-        mqGenericDTO.setMsgId("65777");
-
-        rabbitMQComponet.sendMessage(mqGenericDTO,mqGenericDTO.getRoutingKey(),mqGenericDTO.getExchange());
-    }
 
 
     @org.junit.jupiter.api.Test
